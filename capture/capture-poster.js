0029a180c76adaa640a1dafe27cbe956a3600308
Jacob Madden <jacob.madden@gmail.com>
2015-11-11 16:31:13 +0800

    fix typo in url

--- capture/capture-poster.js
+++ capture/capture-poster.js
@@ -3,7 +3,7 @@ var needle = require('needle');
 var products = require('./data/themeforest-popular-20151110.json').products;
 
 for(var i = 0; i < products.length; i++) {
-  needle.post('http://www.wordpressya.com/api/product/api/product', products[i], function(err, resp, body) {
+  needle.post('http://www.wordpressya.com/api/product', products[i], function(err, resp, body) {
     console.log(body);
     return;
   });
